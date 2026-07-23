d3e7de3ccbae7478990b59a50d81d81c686fc8e8
Kerlandpok <93793807+Kerlandpok@users.noreply.github.com>
2026-07-23 18:12:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="affiches-marche-v1";
-const ASSETS=["./","index.html","manifest.webmanifest","icon.svg"];
+const CACHE="affiches-marche-v2";
+const ASSETS=["./","index.html","manifest.webmanifest","icon.svg","logo-igp.svg","logo-aop.svg","logo-label-rouge.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
