811d6097690517d24b784154df71621539bd1e6d
Kerlandpok <93793807+Kerlandpok@users.noreply.github.com>
2026-07-24 17:36:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="affiches-marche-v2-vraies-photos-galerie-1";
+const CACHE="affiches-marche-v2-bibliotheque-perso-1";
 const ASSETS=["./","index.html","sw.js","logo-igp.svg","logo-aop.svg","logo-label-rouge.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
