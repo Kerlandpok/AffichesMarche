4d9ba3f8de96b7ff2f723c4776d339c1abbc1db8
Kerlandpok <93793807+Kerlandpok@users.noreply.github.com>
2026-07-24 22:00:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="affiches-marche-v2-2-xiaomi14t-20260724";
+const CACHE="affiches-marche-v2-3-xiaomi14t-20260724";
 const ASSETS=["./","index.html","sw.js","logo-igp-officiel.jpg","logo-aop-officiel.jpg","logo-label-rouge-officiel.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
